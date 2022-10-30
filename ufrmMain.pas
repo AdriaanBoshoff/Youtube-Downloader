5f0f48d32d630686b405c35c291f573414d94400
Adriaan Boshoff <adriaanboshoff25@gmail.com>
2022-10-30 02:40:44 +0200

    Handle mp3 and mp4 + REFACTOR

--- ufrmMain.pas
+++ ufrmMain.pas
@@ -3,22 +3,10 @@ unit ufrmMain;
 interface
 
 uses
-  System.SysUtils,
-  System.Types,
-  System.UITypes,
-  System.Classes,
-  System.Variants,
-  FMX.Types,
-  FMX.Controls,
-  FMX.Forms,
-  FMX.Graphics,
-  FMX.Dialogs,
-  FMX.StdCtrls,
-  FMX.Controls.Presentation,
-  udmStyles,
-  FMX.Layouts,
-  FMX.ListBox,
-  FMX.Edit, Skia, Skia.FMX;
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, udmStyles, FMX.Layouts, FMX.ListBox, FMX.Edit, Skia,
+  Skia.FMX, uDownloadListBoxItem;
 
 type
   TfrmMain = class(TForm)
@@ -34,10 +22,12 @@ type
     btnClearURL: TClearEditButton;
     btnDownloadMP4: TButton;
     btnDownloadMP3: TButton;
-    procedure FormCreate(Sender: TObject);
+    procedure btnDownloadMP3Click(Sender: TObject);
+    procedure btnDownloadMP4Click(Sender: TObject);
     procedure OnAppVersionResized(Sender: TObject);
   private
     { Private declarations }
+    procedure DownloadMedia(const URL: string; const MediaType: TDownloadMediaType);
   public
     { Public declarations }
   end;
@@ -47,24 +37,25 @@ var
 
 implementation
 
-uses
-  uDownloadListBoxItem;
-
 {$R *.fmx}
 
-procedure TfrmMain.FormCreate(Sender: TObject);
+procedure TfrmMain.btnDownloadMP3Click(Sender: TObject);
 begin
-  lstDownloads.BeginUpdate;
-  try
-    for var I := 0 to 20 do
-    begin
-      var aItem := TDownloadListBoxItem.Create(lstDownloads);
-      aItem.Tag := I;
-      lstDownloads.AddObject(aItem);
-    end;
-  finally
-    lstDownloads.EndUpdate;
-  end;
+  DownloadMedia(edtURL.Text.Trim, TDownloadMediaType.mtMP3);
+end;
+
+procedure TfrmMain.btnDownloadMP4Click(Sender: TObject);
+begin
+  DownloadMedia(edtURL.Text.Trim, TDownloadMediaType.mtMP4);
+end;
+
+procedure TfrmMain.DownloadMedia(const URL: string; const MediaType: TDownloadMediaType);
+begin
+  var aItem := TDownloadListBoxItem.Create(lstDownloads);
+  aItem.DownloadLink := URL;
+  aItem.DownloadMediaType := MediaType;
+  lstDownloads.AddObject(aItem);
+  aItem.StartDownload;
 end;
 
 procedure TfrmMain.OnAppVersionResized(Sender: TObject);
